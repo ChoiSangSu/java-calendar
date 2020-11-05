1a128474d115d208d7db631227c29f6b5fe16e74
ChoiSangSu <tkdtn1101@naver.com>
2020-11-05 15:17:00 +0900

    Update calendar for

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -7,7 +7,6 @@ public class Calendar {
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public int getmaxDaysofMonth(int month) {
-
 		return MAX_DAYS[month - 1];
 	}
 
@@ -20,19 +19,22 @@ public class Calendar {
 //		System.out.println("22 23 24 25 26 27 28");
 
 		// 숫자를 입력 받아 해당하는 달의 최대 일수를 출력
-		System.out.println("원하는 달을 입력하세요.");
-		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
-		int month = scanner.nextInt();
-		//함수를 이용 
-		cal.getmaxDaysofMonth(month);
-		
-		/* int[] arrary = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; */
-		scanner.close();
-		//System.out.printf("%d는 %d일까지 있습니다", month, arrary[month - 1]);
-		
-		//함수를 이용하여 
-		System.out.printf("%d는 %d일까지 있습니다", month, cal.getmaxDaysofMonth(month));
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("반복 횟수를 선택하세요");
+		int repeat = scanner.nextInt();
+		for (int i = 0; i < repeat; i++) {
+			System.out.println("원하는 달을 입력하세요.");
+			Calendar cal = new Calendar();
+			int month = scanner.nextInt();
+			// 함수를 이용
+			cal.getmaxDaysofMonth(month);
+			/* int[] arrary = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; */
+			// System.out.printf("%d는 %d일까지 있습니다", month, arrary[month - 1]);
+			// 함수를 이용하여
+			System.out.printf("%d는 %d일까지 있습니다 \n", month, cal.getmaxDaysofMonth(month));
+		}
+		System.out.println("Good Bye");
+		scanner.close();
 	}
 }
